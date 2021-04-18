0eaaa8ae4195e17c770a768e0d206d3795c006c0
Shubham Verma <lpushubham98@gmail.com>
2021-04-19 02:11:20 +0530

    jwt auth

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,3 +1,33 @@
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+//handle Errors
+const handleError = (err) => {
+	console.log(err.message, err.code);
+	let errors = { email: "", password: "" };
+
+	//duplicate error code by usings existing email
+	if (err.code === 11000) {
+		errors.email = "Email is already registered";
+		return errors;
+	}
+
+	//validation errors
+	if (err.message.includes("user validation failed")) {
+		Object.values(err.errors).forEach(({ properties }) => {
+			errors[properties.path] = properties.message;
+		});
+	}
+	return errors;
+};
+
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+	return jwt.sign({ id }, "user secret key", {
+		expiresIn: maxAge,
+	});
+};
+
 module.exports.signup_get = (req, res) => {
 	res.render("signup");
 };
@@ -5,11 +35,22 @@ module.exports.signup_get = (req, res) => {
 module.exports.login_get = (req, res) => {
 	res.render("login");
 };
-module.exports.signup_post = (req, res) => {
-	res.send("new signup");
+module.exports.signup_post = async (req, res) => {
+	const { email, password } = req.body;
+
+	try {
+		const user = await User.create({ email, password });
+		const token = createToken(user._id);
+		res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+		res.status(201).json({user: user._id});
+	} 
+	catch (err) {
+		const errors = handleError(err);
+		res.status(400).json({ errors });
+	}
 };
 module.exports.login_post = (req, res) => {
-	console.log(req.body);
-	//const { email, password } = req.body;
+	const { email, password } = req.body;
+	console.log(email, password);
 	res.send("user Login");
 };
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 const app = express();
 const PORT = 3000;
 
 //middlewares
 app.use(express.static("public"));
-app.use(authRoutes);
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 //view engines
 app.set("view engine", "ejs");
@@ -32,3 +32,23 @@ mongoose
 //routes
 app.get("/", (req, res) => res.render("home"));
 app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.use(authRoutes);
+
+//cookies
+app.get("/set-cookies", (req, res) => {
+	//res.setHeader("Set-Cookie", "newUser=true");
+
+	res.cookie("newUser", false);
+	res.cookie("isEmployed", true, {
+		maxAge: 1000 * 60 * 60 * 24,
+		httpOnly: true,
+	});
+
+	res.send("You got the cookies!");
+});
+
+app.get("/read-cookies", (req, res) => {
+	const cookies = req.cookies;
+	console.log(cookies);
+	res.json(cookies);
+});
